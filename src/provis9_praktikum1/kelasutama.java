322dfd20a6165db8c451cc3d29355dd9826dd9b0
vaniamzf <vaniamzf@gmail.com>
2020-04-01 16:01:25 +0700

    Tugas 1

---
+++ src/provis9_praktikum1/kelasutama.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provis9_praktikum1;
+
+/**
+ *
+ * @author ACER
+ */
+public class kelasutama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        futama futama = new futama();
+        futama.setVisible(true);
+    }
+    
+}
